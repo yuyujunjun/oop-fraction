379fb36ab4ee3be6e33e68a5819676a31b10971a
yunuohanxue <fcsx1tf@163.com>
2016-12-01 19:50:58 +0800

    overload << and >> to complete the fraction

--- main.cpp
+++ main.cpp
@@ -15,6 +15,25 @@ fraction::fraction(int d):denominator(d),numberator(1){
 fraction::fraction():denominator(0),numberator(1){
     reduction();
 }
+istream& operator>>(istream& in,fraction& com)
+{
+    cout<<"import the denominator"<<endl;
+    in>>com.denominator;
+    cout<<"import the numberator"<<endl;
+    in>>com.numberator;
+    com.reduction();
+    return in;
+}
+ostream& operator<<(ostream& os,const fraction& com)
+{
+    if(com.numberator!=1&&com.denominator!=0)
+        os<<com.denominator<<"/"<<com.numberator<<endl;
+    else if(com.numberator==1)
+        os<<com.denominator<<endl;
+    else if(com.denominator==0)
+        os<<0<<endl;
+    return os;
+}
  const fraction fraction:: operator-()const{
     fraction temp(-denominator,numberator);
      return temp;
@@ -101,44 +120,27 @@ bool operator<=(fraction const &a,fraction const &b)
 {
     return !(a>b);
 }
-const void fraction:: print()const
-{
-    if(numberator!=1&&denominator!=0)
-        cout<<denominator<<"/"<<numberator<<endl;
-    else if(numberator==1)
-        cout<<denominator<<endl;
-    else if(denominator==0)
-        cout<<0<<endl;
+//const void fraction:: print()const
+//{
 
-}
+
+//}
 int main() {//get some exam
-    srand((int)time(0));
-    int numerator=random();
-    int denomintor=random();
-    cout<<"Test output looks like:2, 2/4, 0/1, 2/0"<<endl;
-    fraction(2).print();
-    fraction(2,4).print();
-    fraction(0,1).print();
+    cout<<"Test output looks like:2, -2/-4, 0/1, 2/0"<<endl;
+    cout<<fraction(2);
+    cout<<fraction(-2,-4);
+    cout<<fraction(0,1);
     //fraction(2,0).print();
     fraction f1;
-    f1=denomintor;
-    cout<<"the first random fraction by "<<denomintor<<" and "<< "1 : ";
-    f1.print();
-    numerator=random()*(-3);//for display the fraction<0 and reduction
-    denomintor=random()*6;
-    fraction f2(denomintor,numerator);
-    cout<<"the second random fraction by "<<denomintor<<" and "<<numerator<<" :  ";
-    f2.print();
-    cout<<"f1+f2=";
-    (f1+f2).print();
-    cout<<"f1*f2=";
-    (f1*f2).print();
-    cout<<"f1/f2=";
-    (f1/f2).print();
-    cout<<"-f2=";
-    (-f2).print();
-    cout<<"the reciprocal f2=";
-    f2.reciprocal().print();
+    fraction f2;
+    cin>>f1>>f2;
+    cout<<"the first fraction f1:"<<f1<<endl;
+    cout<<"the second fraction f2:"<<f2<<endl;
+    cout<<"f1+f2="<<f1+f2<<endl;
+    cout<<"f1*f2="<<f1*f2<<endl;
+    cout<<"f1/f2="<<f1/f2<<endl;
+    cout<<"-f2="<<-f2<<endl;
+    cout<<"the reciprocal f2="<<f2.reciprocal();
     cout<<"f1<f2:"<<(f1<f2)<<endl;
     cout<<"f1<=f2:"<<(f1<=f2)<<endl;
     cout<<"f1>f2:"<<(f1>f2)<<endl;
